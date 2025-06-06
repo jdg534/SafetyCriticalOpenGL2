acba4b80adc74d1e84e4db819de1ad7f6a63bd75
Josh Gibson <joshdgibson21@outlook.com>
2025-06-06 22:57:17 +0100

    Added basic glfw window.

--- source/library/library_main.cpp
+++ source/library/library_main.cpp
@@ -4,7 +4,47 @@
 
 #include <gl2platform.h>
 
+#include <exception>
+
+// public
+/////////
+
 void library_main::run()
 {
-	std::cout << "Hello world." << std::endl;
+	initialise();
+	while (!glfwWindowShouldClose(m_window))
+	{
+		// todo add basic rotating triangle.
+		glfwPollEvents();
+	}
+	shutdown();
+}
+
+// private
+//////////
+
+void library_main::initialise()
+{
+	if (glfwInit() != GLFW_TRUE)
+	{
+		throw std::exception("glfwInit() failed");
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // todo figure out correct values of SC 2.0
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // ''
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // ''
+
+	m_window = glfwCreateWindow(800, 600, "A window", nullptr, nullptr);
+	if (m_window == nullptr)
+	{
+		throw std::exception("glfwCreateWindow() failed");
+	}
+}
+
+void library_main::shutdown()
+{
+	if (m_window)
+	{
+		glfwDestroyWindow(m_window);
+		m_window = nullptr;
+	}
 }
--- source/library/library_main.h
+++ source/library/library_main.h
@@ -1,6 +1,7 @@
 #pragma once
 // HI C++ might not accept this.if so go to #ifndef #define #endif
 
+#include <GLFW/glfw3.h>
 
 // class for 
 class library_main
@@ -9,5 +10,11 @@ public:
 
 	void run();
 
+private:
+
+	void initialise();
+	void shutdown();
+
+	GLFWwindow* m_window { nullptr };
 };
 
