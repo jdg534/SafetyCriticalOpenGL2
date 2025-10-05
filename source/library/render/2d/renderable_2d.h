2d77c10a82dfccd247b5c4ed1952623023dcf5c9
Josh Gibson <joshdgibson21@outlook.com>
2025-10-05 14:09:45 +0100

    Planning...

---
+++ source/library/render/2d/renderable_2d.h
@@ -0,0 +1,17 @@
+#ifndef _RENDERABLE_2D_H_
+#define _RENDERABLE_2D_H_
+
+/* to cover:
+* quad
+* text_block (Array of quads texture from font atlas)
+*/
+
+/* need:
+* vertex structure (Vec2d pos, Vec2d tex_coords, Vec3d blend_colour)
+* vertex buffer id
+* index buffer id and range 
+* texture id
+* shader (program) id
+*/
+
+#endif // _RENDERABLE_2D_H_
---
+++ source/library/render/3d/renderable_3d.h
@@ -0,0 +1,17 @@
+#ifndef _RENDERABLE_3D_H_
+#define _RENDERABLE_3D_H_
+
+// this is a placeholder.
+// This will be the base class for geomentry.
+// terrain will be another class (inheriting from this)
+
+/* need:
+* vertex structure (Vec3d pos, Vec3d normal, Vec2d tex_coords, Vec3d blend_colour)
+* vertex buffer id
+* index buffer id and range
+* texture id
+* shader (program) id
+*/
+
+
+#endif // _RENDERABLE_3D_H_
