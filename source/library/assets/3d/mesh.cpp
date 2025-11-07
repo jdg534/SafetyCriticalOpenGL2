5d132f9f3fe1d85617c11bf6a9e9325487cac0c6
Josh Gibson <joshdgibson21@outlook.com>
2025-11-07 23:43:44 +0000

    Added static mesh loading to GPU.

--- source/library/assets/3d/mesh.cpp
+++ source/library/assets/3d/mesh.cpp
@@ -8,8 +8,10 @@
 #include <assimp/scene.h>
 
 #include "../../render/vertex_types.h"
+#include "../../render/include_opengl.h" // not solid correct.
 
 using namespace std;
+using namespace gl;
 
 // public
 /////////
@@ -36,7 +38,7 @@ void mesh::initialise_assimp_struct(const aiMesh* initialise_with)
 
     if (initialise_with->HasBones())
     {
-        throw runtime_error("Attempted to load rigged mesh, this isn't currently supported");
+        throw runtime_error("Attempted to load rigged mesh, this isn't currently supported"); // if there's demand 
     }
     
     if (!initialise_with->HasTextureCoords(0) || !initialise_with->HasPositions() || !initialise_with->HasNormals())
@@ -44,51 +46,94 @@ void mesh::initialise_assimp_struct(const aiMesh* initialise_with)
         throw runtime_error("Only meshes with normals and texture coordinates are supported");
     }
 
-    std::vector<vertex_3d> vertex_buffer_init_data;
-    std::vector<unsigned short> index_buffer_init_data;
+    std::vector<vertex_3d> vertex_buffer_data;
+    std::vector<unsigned short> index_buffer_data;
 
-    vertex_buffer_init_data.resize(initialise_with->mNumVertices);
-    index_buffer_init_data.resize(initialise_with->mNumFaces * 3);
+    vertex_buffer_data.resize(initialise_with->mNumVertices);
+    index_buffer_data.resize(initialise_with->mNumFaces * 3);
+
+    const GLsizeiptr vertex_buffer_size = vertex_buffer_data.size() * vertex3d_struct_size;
+    const GLsizeiptr index_buffer_size = index_buffer_data.size() * sizeof(unsigned short);
 
     for (unsigned int i = 0; i < initialise_with->mNumVertices; ++i)
     {
-        vertex_buffer_init_data[i].position.x = initialise_with->mVertices[i].x;
-        vertex_buffer_init_data[i].position.y = initialise_with->mVertices[i].y;
-        vertex_buffer_init_data[i].position.z = initialise_with->mVertices[i].z;
+        vertex_buffer_data[i].position.x = initialise_with->mVertices[i].x;
+        vertex_buffer_data[i].position.y = initialise_with->mVertices[i].y;
+        vertex_buffer_data[i].position.z = initialise_with->mVertices[i].z;
 
-        vertex_buffer_init_data[i].normal.x = initialise_with->mNormals[i].x;
-        vertex_buffer_init_data[i].normal.y = initialise_with->mNormals[i].y;
-        vertex_buffer_init_data[i].normal.z = initialise_with->mNormals[i].z;
+        vertex_buffer_data[i].normal.x = initialise_with->mNormals[i].x;
+        vertex_buffer_data[i].normal.y = initialise_with->mNormals[i].y;
+        vertex_buffer_data[i].normal.z = initialise_with->mNormals[i].z;
 
-        vertex_buffer_init_data[i].texture_coordinates.x = initialise_with->mTextureCoords[0][i].x;
-        vertex_buffer_init_data[i].texture_coordinates.y = initialise_with->mTextureCoords[0][i].y;
+        vertex_buffer_data[i].texture_coordinates.x = initialise_with->mTextureCoords[0][i].x;
+        vertex_buffer_data[i].texture_coordinates.y = initialise_with->mTextureCoords[0][i].y;
     }
 
     for (unsigned int i = 0; i < initialise_with->mNumFaces; ++i)
     {
         const aiFace& face = initialise_with->mFaces[i];
-        if (!face.mNumIndices == 3)
+        if (face.mNumIndices != 3)
         {
             throw runtime_error("Only triangular meshes are supported!");
         }
         const unsigned int write_index = i * 3;
-        index_buffer_init_data[write_index + 0] = face.mIndices[0];
-        index_buffer_init_data[write_index + 1] = face.mIndices[1];
-        index_buffer_init_data[write_index + 2] = face.mIndices[2];
+        index_buffer_data[write_index + 0] = face.mIndices[0];
+        index_buffer_data[write_index + 1] = face.mIndices[1];
+        index_buffer_data[write_index + 2] = face.mIndices[2];
     }
 
-    throw runtime_error("TODO: finish setting up the index and vertex buffers, once they're on the GPU no need to keep them in normal RAM");
+    glGenVertexArrays(1, &m_vertex_array_id);
+    glBindVertexArray(m_vertex_array_id);
+
+    // make the buffer, want to get to off set.
+    GLuint buffer_ids[2];
+    glGenBuffers(2, buffer_ids); // [0] vertex buffer, [1] index buffer
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_ids[0]);
+    glBufferData(GL_ARRAY_BUFFER, vertex_buffer_size, vertex_buffer_data.data(), GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer_ids[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_buffer_size, index_buffer_data.data(), GL_STATIC_DRAW);
+
+    // layout(location = 0): vec3 position
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, vertex3d_struct_size, (void*)offsetof(vertex_3d, position));
+
+    // layout(location = 1): vec2 uv
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, vertex3d_struct_size, (void*)offsetof(vertex_3d, texture_coordinates));
+
+    // layout(location = 2) in vec3 normal;
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_TRUE, vertex3d_struct_size, (void*)offsetof(vertex_3d, normal));
+
+    m_vertex_buffer_id = buffer_ids[0];
+    m_index_buffer_id = buffer_ids[1];
+    m_index_element_count = index_buffer_data.size();
+    m_material_index = initialise_with->mMaterialIndex;
+
+    // Unbind VAO (safe cleanup)
+    glBindVertexArray(0);
 }
 
 void mesh::shutdown()
 {
-	// TODO: CODE ME!
+    GLuint buffer_ids[2]{ m_vertex_buffer_id, m_index_buffer_id };
+    glDeleteBuffers(2, buffer_ids);
+    glDeleteVertexArrays(1, &m_vertex_array_id);
+    m_vertex_array_id = m_vertex_buffer_id = m_index_buffer_id = m_index_buffer_id = 0;
 }
 
 asset_type mesh::get_type() const
 {
 	return asset_type::mesh;
 }
+
+gl::GLuint mesh::get_vertex_array_id() const { return m_vertex_array_id; }
+gl::GLuint mesh::get_vertex_buffer_id() const { return m_vertex_buffer_id; }
+gl::GLuint mesh::get_index_buffer_id() const { return m_index_buffer_id; }
+gl::GLuint mesh::get_index_element_count() const { return m_index_buffer_id; }
+gl::GLint  mesh::get_material_index() const { return m_material_index; }
 
 // private
 //////////
--- source/library/assets/3d/mesh.h
+++ source/library/assets/3d/mesh.h
@@ -6,9 +6,13 @@
 #include <string_view>
 
 #include <assimp/mesh.h>
+#include <glbinding/gl/types.h>
 
 #include "../asset.h"
 #include "material.h"
+
+// this class is just meant to own the data and buffers. It's NOT responsible for rendering.
+// seperate classes are to be setup to do that.
 
 class mesh : public asset
 {
@@ -23,8 +27,19 @@ public:
 	void shutdown() override;
 	asset_type get_type() const override;
 
+	gl::GLuint get_vertex_array_id() const;
+	gl::GLuint get_vertex_buffer_id() const;
+	gl::GLuint get_index_buffer_id() const;
+	gl::GLuint get_index_element_count() const;
+	gl::GLint  get_material_index() const;
+
 private:
 
+	gl::GLuint m_vertex_array_id { 0 };
+	gl::GLuint m_vertex_buffer_id { 0 };
+	gl::GLuint m_index_buffer_id { 0 };
+	gl::GLuint m_index_element_count{ 0 };
+	gl::GLint  m_material_index { -1 };
 };
 
 #endif // _MESH_H_
