993c0aa9602320f7998c059a2fa623eeb96bbf83
Josh Gibson <joshdgibson21@outlook.com>
2026-04-20 00:02:32 +0100

    Updated clang-tidy usage.

--- run_clang_tidy.sh
+++ run_clang_tidy.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 cmake --preset=static-analysis
 
 BUILD_DIR="./static_analysis"
 COMPILE_DB="$BUILD_DIR/compile_commands.json"
 
-FAIL=0
-
 while IFS= read -r file; do
-    clang-tidy "$file" -p "$BUILD_DIR" --warnings-as-errors='*' || FAIL=1
+	windows_style_path=$(cygpath -m "$file")
+    clang-tidy "$windows_style_path" -p "$BUILD_DIR"
 done < <(jq -r '.[].file | select(contains("submodules") | not)' "$COMPILE_DB")
 
-exit $FAIL
