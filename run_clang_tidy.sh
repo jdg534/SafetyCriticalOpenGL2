b816bc475a47fabcb395598dfd52ac2974c41d45
Josh Gibson <joshdgibson21@outlook.com>
2026-04-19 23:51:31 +0100

    The checks filter out submodules.

--- run_clang_tidy.sh
+++ run_clang_tidy.sh
@@ -10,6 +10,6 @@ FAIL=0
 
 while IFS= read -r file; do
     clang-tidy "$file" -p "$BUILD_DIR" --warnings-as-errors='*' || FAIL=1
-done < <(jq -r '.[].file' "$COMPILE_DB")
+done < <(jq -r '.[].file | select(contains("submodules") | not)' "$COMPILE_DB")
 
 exit $FAIL
